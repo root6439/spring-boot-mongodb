a9f42f0d74003c4b76bb6ed72b0f8f86983da570
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-19 18:27:01 -0300

    Testando

--- src/main/java/com/root6439/springmongodb/resources/UserResource.java
+++ src/main/java/com/root6439/springmongodb/resources/UserResource.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.root6439.springmongodb.domain.User;
@@ -14,7 +14,7 @@ import com.root6439.springmongodb.domain.User;
 	@RestController
 	@RequestMapping(value = "/users")
 	public class UserResource {
-		@GetMapping
+		@RequestMapping(method = RequestMethod.GET)
 		public ResponseEntity<List<User>> findAll() {
 			List<User> list = new ArrayList<>();
 			User maria = new User("1001", "Maria Brown", "maria@gmail.com");
