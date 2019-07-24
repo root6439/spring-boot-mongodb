34716c4080354f18380d68019cfce5e7a658f137
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-23 21:49:59 -0300

    Obtendo post por ID

---
+++ src/main/java/com/root6439/springmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.root6439.springmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.root6439.springmongodb.domain.Post;
+import com.root6439.springmongodb.repository.PostRepository;
+import com.root6439.springmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+}
