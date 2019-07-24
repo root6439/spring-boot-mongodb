00c085ac01a41b450ace8228e9d6e882f85e1832
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-23 22:28:36 -0300

    Consulta simples

--- src/main/java/com/root6439/springmongodb/repository/PostRepository.java
+++ src/main/java/com/root6439/springmongodb/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.root6439.springmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.root6439.springmongodb.domain.Post;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
