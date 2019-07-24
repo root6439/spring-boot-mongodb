fc08d009b9fb5c16000d500f21bc513364a9b1f7
root6439 <nicolas.soaresh.98@gmail.com>
2019-07-23 22:00:26 -0300

    Posts com comentários

--- src/main/java/com/root6439/springmongodb/config/Instantiation.java
+++ src/main/java/com/root6439/springmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.root6439.springmongodb.domain.Post;
 import com.root6439.springmongodb.domain.User;
 import com.root6439.springmongodb.dto.AuthorDTO;
+import com.root6439.springmongodb.dto.CommentDTO;
 import com.root6439.springmongodb.repository.PostRepository;
 import com.root6439.springmongodb.repository.UserRepository;
 
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("12/03/2018"), "Partiu viagem", "Vou viajar para São Paulo!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/04/2019"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viajem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
